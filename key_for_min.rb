6d71ca8265ed686e37e09742f1e2dd8a81cb3bef
Kevin <kevinlai76@gmail.com>
2019-05-28 06:33:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,10 +10,10 @@ def key_for_min_value(name_hash)
   name_hash.each do |k,v|
     if min === 0
       min === v
-      name === k
+      name += k
     elsif v < min
       v === min            
-      name === k
+      
     end
   end 
   
